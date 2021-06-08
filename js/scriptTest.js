ed452a6dc8a67edafefc116c1f42322f5f4100a0
Andrii Lypovetskyi <li_andr@ukr.net>
2021-06-08 19:31:15 +0300

    Update

--- js/scriptTest.js
+++ js/scriptTest.js
@@ -1,24 +1,8 @@
-// / Класс со статическими свойствами и методами
-class Calc {
-  // Класс-калькулятор для двух аргументов
-  constructor() {}
-
-  // Метод как замена свойству
-  static get PI() {
-    return 3.14;
-  }
-
-  // Статический метод +
-  static add(...args) {
-    return args.reduce((acc, next) => acc + next, 0);
-  }
-
-  // Статический метод *
-  static mult(...args) {
-    return args.reduce((acc, next) => acc * next, 1);
-  }
-}
-
-console.log(Calc.PI); // 3.14
-console.log(Calc.add(2, 3, 4)); // 9
-console.log(Calc.mult(12, 3, 4)); // 144
+const a = {};
+const b = {key: 'b'};
+const c = {key: 'c'};
+a[b] = 1;
+a[c] = 2;
+console.log(a[c]);
+console.log(b);
+console.log(c);
